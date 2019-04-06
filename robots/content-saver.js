9bd21878bef5e05c8bc676b8797958b5462c8c42
Gabriel <gabriel_luchtenberg@edu.univali.br>
2019-04-06 14:15:10 -0300

    trabalho finalizado

---
+++ robots/content-saver.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const contentFilePath = "./result.txt";
+const separator = "\r\n";
+function save(data) {
+  const content = data.join(separator);
+  return fs.writeFileSync(contentFilePath, content);
+}
+
+function load() {
+  const fileBuffer = fs.readFileSync(contentFilePath, "utf-8");
+  const jsonContent = fileBuffer.split(separator);
+  return jsonContent;
+}
+
+module.exports = {
+  save,
+  load
+};
